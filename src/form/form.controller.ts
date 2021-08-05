05815050980a8e10bb4d4ff57f89c044b3d46805
MariaRange <ticqraeg001125mdgnsda1@gmail.com>
2021-08-05 02:02:28 -0500

    cambio de correo

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -12,7 +12,7 @@ export class FormController {
     try {
       const info = await this.mailerServive.transporter.sendMail({
         from: `"Cliente👻" <${body.email}>`, // sender address
-        to: 'alexis.riosc2701@gmail.com, maria.rangel.is@unipolidgo.edu.mx', // list of receivers
+        to: 'laboratoriodentalflores1@gmail.com', // list of receivers
         subject: 'Cliente', // Subject line
         text: `
           Telefono: ${body.phone}
